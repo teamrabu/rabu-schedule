ee11365c1b4c1745c6ab88fb05029eb4c7c282bb
James Shore <jshore@jamesshore.com>
2011-03-09 13:17:14 -0800

    Very basic DOM manipulation from within test

--- serve.sh
+++ serve.sh
@@ -1,1 +1,2 @@
+open http://localhost:4224/capture?strict
 java -jar tools/JsTestDriver-1.3.1.jar --port 4224
--- src/_hello_test.js
+++ src/_hello_test.js
@@ -1,6 +1,10 @@
 GreeterTest = TestCase("GreeterTest");
 
 GreeterTest.prototype.testGreet = function() {
-  var greeter = new myapp.Greeter();
-  assertEquals("Hello World!", greeter.greet("World"));
+    var greeter = new myapp.Greeter();
+    assertEquals("Hello World!", greeter.greet("World"));
+//    assertTrue("Console output: \n[" + document.URL + "]\n", false);
+
+    body = document.getElementsByTagName("body")[0];
+    body.innerHTML = "fodo!";
 };
