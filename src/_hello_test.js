5ca8f0cf8a77e3ae796652e7ccbbf2470cbf0531
James Shore <jshore@jamesshore.com>
2011-03-09 16:27:59 -0800

    Various stabs at figuring out testing

--- src/_hello_test.js
+++ src/_hello_test.js
@@ -5,6 +5,19 @@ GreeterTest.prototype.testGreet = function() {
     assertEquals("Hello World!", greeter.greet("World"));
 //    assertTrue("Console output: \n[" + document.URL + "]\n", false);
 
-    body = document.getElementsByTagName("body")[0];
-    body.innerHTML = "fodo!";
+    var body = document.getElementsByTagName("body")[0];
+
+//    body.innerHTML = "tag: <p id='my_para'>para</p>";
+//    var my_para = document.getElementById("my_para");
+//    assertEquals("P", my_para.tagName);
+
+    body.innerHTML = "<iframe id='bob' src='http://localhost:4224' ></iframe>"
+    ;
+    var bob = document.getElementById("bob");
+    console.log(bob.innerHTML);
+    bob.onload = function() {
+    };
+    assertFalse("failure!");
+
+
 };
