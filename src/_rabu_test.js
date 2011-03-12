a3b531843d88ea1975d09ac84ed29ec41b1fec12
James Shore <jshore@jamesshore.com>
2011-03-11 19:50:38 -0800

    Basic projection calculation

--- src/_rabu_test.js
+++ src/_rabu_test.js
@@ -1,18 +1,36 @@
-/*global Rabu, TestCase, assertEquals */
+/*global Rabu, TestCase, assertSame, objectifyConfig */
 
 var RabuTest = new TestCase("Rabu");
 
 RabuTest.prototype.setUp = function() {
 	this.config = {
-		title: "My title"
+		title: "My title",
+		riskMultipliers: [1, 2, 4],
+		velocity: 10,
+		effortRemaining: 100
 	};
+	this.rabu = new Rabu(this.config);
 };
 
-RabuTest.prototype.testTitle = function() {
+RabuTest.prototype.test_title = function() {
 	/*:DOC += <h1 id="title"></h1> */
 
-	var rabu = new Rabu(this.config);
-	rabu.populateDom();
+	this.rabu.populateDom();
+	assertSame("My title", $("#title").text());
+};
 
-	assertEquals("My title", $("#title").text());
+RabuTest.prototype.test_projections = function() {
+	assertSame("10%", 10, this.rabu.tenPercentIterationsRemaining());
+	assertSame("50%", 20, this.rabu.fiftyPercentIterationsRemaining());
+	assertSame("90%", 40, this.rabu.ninetyPercentIterationsRemaining());
+};
+
+RabuTest.prototype.test_projectionsShouldWorkWithNonIntegerValues = function() {
+	this.config.riskMultipliers = [0.6, 1.4, 1.8];
+	this.config.velocity = 9.5;
+	this.config.effortRemaining = 73;
+
+	assertSame("10%", 4.610526315789474, this.rabu.tenPercentIterationsRemaining());
+	assertSame("50%", 10.757894736842104, this.rabu.fiftyPercentIterationsRemaining());
+	assertSame("90%", 13.83157894736842, this.rabu.ninetyPercentIterationsRemaining());
 };
--- src/rabu.js
+++ src/rabu.js
@@ -1,10 +1,45 @@
-var Rabu = function(config) {
-	if (!config) {
-		throw "Expected config";
-	}
-	this.config = config;
-};
+(function() {
+	var config;
 
-Rabu.prototype.populateDom = function() {
-	$("#title").text(this.config.title);
-};
+	function decorateConfig() {
+		config.tenPercentMultiplier = function() {
+			return config.riskMultipliers[0];
+		};
+		config.fiftyPercentMultiplier = function() {
+			return config.riskMultipliers[1];
+		};
+		config.ninetyPercentMultiplier = function() {
+			return config.riskMultipliers[2];
+		};
+	}
+
+	function iterations() {
+		return config.effortRemaining / config.velocity;
+	}
+
+	function Rabu(config_in) {
+		if (!config_in) {
+			throw "Expected config";
+		}
+		config = config_in;
+		decorateConfig();
+	}
+
+	Rabu.prototype.populateDom = function() {
+		$("#title").text(config.title);
+	};
+
+	Rabu.prototype.tenPercentIterationsRemaining = function() {
+		return iterations() * config.tenPercentMultiplier();
+	};
+
+	Rabu.prototype.fiftyPercentIterationsRemaining = function() {
+		return iterations() * config.fiftyPercentMultiplier();
+	};
+
+	Rabu.prototype.ninetyPercentIterationsRemaining = function() {
+		return iterations() * config.ninetyPercentMultiplier();
+	};
+
+	this.Rabu = Rabu;
+}());
