35a09889ccaadf4a9eb25ce266de7ad3496ec1b1
James Shore <jshore@jamesshore.com>
2011-04-27 11:31:51 -0700

    Extracted 'Projection' from 'Projections'

--- src/javascript/_projections_test.js
+++ src/javascript/_projections_test.js
@@ -1,63 +1,48 @@
 // Copyright (C) 2011 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
 
 (function() {
-	var Test = new TestCase("Projections");
-	var projections;
-	var config;
-
+	var Test = new TestCase("Projection");
+	var rs = rabu.schedule;
+	var projection, iteration, config;
+	
 	Test.prototype.setUp = function() {
 		config = {
-			name: "My name",
-			updated: "5 Jan 2011",
-			riskMultipliers: [1, 2, 4],
-			iterations: [{
-				started: "1 Jan 2011",
-				length: 7,
-				velocity: 10,
-				included: [
-					["completed", 0],
-					["feature A", 70],
-					["feature B", 30]
-				],
-				excluded: [
-					["excluded 1", 20]
-				]
-			}]
+			started: "1 Jan 2011",
+			length: 7,
+			velocity: 10,
+			included: [
+				["feature", 100]
+			]
 		};
-		projections = new rabu.schedule.Projections(new rabu.schedule.Estimates(config));
+		iteration = new rs.Iteration(config);
 	};
+	
+	Test.prototype.test_iterationsRemaining = function() {
+		projection = new rs.Projection(iteration, 2);
+		assertEquals("basic calculation", 20, projection.iterationsRemaining());
 
-	Test.prototype.test_iterationProjections = function() {
-		assertEquals("10%", 10, projections.tenPercentIterationsRemaining());
-		assertEquals("50%", 20, projections.fiftyPercentIterationsRemaining());
-		assertEquals("90%", 40, projections.ninetyPercentIterationsRemaining());
+		projection = new rs.Projection(iteration, 0.25);		
+		assertEquals("should not round", 2.5, projection.iterationsRemaining());
 	};
-
-	Test.prototype.test_dateProjections = function() {
-		assertEquals("10%", new Date("12 Mar 2011"), projections.tenPercentDate());
-		assertEquals("50%", new Date("21 May 2011"), projections.fiftyPercentDate());
-		assertEquals("90%", new Date("8 Oct 2011"), projections.ninetyPercentDate());
+	
+	Test.prototype.test_daysRemaining = function() {
+		projection = new rs.Projection(iteration, 1);
+		assertEquals("basic calculation", 70, projection.daysRemaining());
+		
+		projection = new rs.Projection(iteration, 0.25);
+		assertEquals("should round up to next day", 18, projection.daysRemaining());
 	};
-
-	Test.prototype.test_iterationProjectionsShouldNotRound = function() {
-		config.riskMultipliers = [0.6, 1.4, 1.6];
-		config.iterations[0].velocity = 9.5;
-		config.iterations[0].included = [["A", 73]];
-
-		assertEquals("10%", 4.610526315789474, projections.tenPercentIterationsRemaining());
-		assertEquals("50%", 10.757894736842104, projections.fiftyPercentIterationsRemaining());
-		assertEquals("90%", 12.294736842105264, projections.ninetyPercentIterationsRemaining());
+	
+	Test.prototype.test_date = function() {
+		projection = new rs.Projection(iteration, 0.1);
+		assertEquals("date", new Date("8 Jan 2011"), projection.date());
 	};
-
-	Test.prototype.test_dateProjectionsShouldRoundUpToNextDay = function() {
-		config.iterations[0].included = [["A", 14]];
-		config.iterations[0].length = 1;
-
-		assertEquals("10%", 1.4, projections.tenPercentIterationsRemaining());
-		assertEquals("50%", 2.8, projections.fiftyPercentIterationsRemaining());
-		assertEquals("90%", 5.6, projections.ninetyPercentIterationsRemaining());
-		assertEquals("10%", new Date("3 Jan 2011"), projections.tenPercentDate());
-		assertEquals("50%", new Date("4 Jan 2011"), projections.fiftyPercentDate());
-		assertEquals("90%", new Date("7 Jan 2011"), projections.ninetyPercentDate());
+	
+	Test.prototype.test_dateRoundedToIteration = function() {
+		projection = new rs.Projection(iteration, 0.11);
+		assertEquals("should round up to beginning of iteration", new Date("15 Jan 2011"), projection.dateRoundedToIteration());
+		
+		projection = new rs.Projection(iteration, 0.1);
+		assertEquals("should not add time when no fraction", new Date("8 Jan 2011"), projection.dateRoundedToIteration());
 	};
 }());
--- src/javascript/projections.js
+++ src/javascript/projections.js
@@ -1,43 +1,68 @@
 // Copyright (C) 2011 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
 
 rabu.schedule.Projections = function(estimates) {
-
-	function iterations() {
-		return estimates.totalEstimate() / estimates.velocity();
+	var rs = rabu.schedule;
+	
+	function tenPercent() {
+		return new rs.Projection(estimates.currentIteration(), estimates.tenPercentMultiplier()); 
 	}
-
-	function calcProjection(multiplier) {
-		return iterations() * multiplier;
+	
+	function fiftyPercent() {
+		return new rs.Projection(estimates.currentIteration(), estimates.fiftyPercentMultiplier());
 	}
-
-	function convertToDate(iterationsRemaining) {
-		var days = Math.ceil(iterationsRemaining * estimates.iterationLength());
-		var date = estimates.currentIterationStarted();
-		date.setDate(date.getDate() + days);
-		return date;
+	
+	function ninetyPercent() {
+		return new rs.Projection(estimates.currentIteration(), estimates.ninetyPercentMultiplier());
 	}
 
 	this.tenPercentDate = function() {
-		return convertToDate(this.tenPercentIterationsRemaining());
+		return tenPercent().date();
 	};
 
 	this.fiftyPercentDate = function() {
-		return convertToDate(this.fiftyPercentIterationsRemaining());
+		return fiftyPercent().date();
 	};
 
 	this.ninetyPercentDate = function() {
-		return convertToDate(this.ninetyPercentIterationsRemaining());
+		return ninetyPercent().date();
 	};
 
 	this.tenPercentIterationsRemaining = function() {
-		return calcProjection(estimates.tenPercentMultiplier());
+		return tenPercent().iterationsRemaining();
 	};
 
 	this.fiftyPercentIterationsRemaining = function() {
-		return calcProjection(estimates.fiftyPercentMultiplier());
+		return fiftyPercent().iterationsRemaining();
 	};
 
 	this.ninetyPercentIterationsRemaining = function() {
-		return calcProjection(estimates.ninetyPercentMultiplier());
+		return ninetyPercent().iterationsRemaining();
+	};
+};
+
+rabu.schedule.Projection = function(iteration, riskMultiplier) {
+	var self = this;
+	
+	function daysToDate(days) {
+		var date = iteration.startDate();
+		date.setDate(date.getDate() + days);
+		return date;
+	}
+	
+	this.iterationsRemaining = function() {
+		return iteration.totalEstimate() / iteration.velocity() * riskMultiplier; 
+	};
+	
+	this.daysRemaining = function() {
+		return Math.ceil(self.iterationsRemaining() * iteration.length()); 
+	};
+	
+	this.date = function() {
+		return daysToDate(self.daysRemaining());
+	};
+	
+	this.dateRoundedToIteration = function() {
+		var days = Math.ceil(self.iterationsRemaining()) * iteration.length();
+		return daysToDate(days);
 	};
 };
