b1110fc6c79af7f094aecb75914adcd37dcd41bd
James Shore <jshore@jamesshore.com>
2011-05-29 16:50:22 -0700

    Rakefile now compiles Java code

--- src/cli/src/com/teamrabu/schedule/cli/Main.java
+++ src/cli/src/com/teamrabu/schedule/cli/Main.java
@@ -1,12 +1,21 @@
 package com.teamrabu.schedule.cli;
 
+import java.io.*;
+
 public class Main {
 	private static String CHARSET = "UTF-8";
 
 	public static void main(String[] args) throws Exception {
-		String template = SimpleFile.loadFromStream(Main.class.getResourceAsStream("projection.html"), CHARSET);
+		InputStream templateStream = Main.class.getResourceAsStream("projection.html");
+		if (templateStream == null) throw new MissingTemplateException();
+
+		String template = SimpleFile.loadFromStream(templateStream, CHARSET);
 		String config = SimpleFile.loadFromStream(System.in, CHARSET);
 
 		System.out.print(new Generator(template, config).html());
 	}
+}
+
+class MissingTemplateException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
 }
