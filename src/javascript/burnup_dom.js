3ad517adbd5ee07730f1689c9b537ec116501ccd
James Shore <jshore@jamesshore.com>
2011-06-24 21:29:04 +0200

    Finished migrating to rs.Date

--- src/javascript/burnup_dom.js
+++ src/javascript/burnup_dom.js
@@ -263,7 +263,6 @@
 	            xTickLabelHeight: this.xTickLabel.getBBox().height,
 				yTickLabelWidth: this.yTickLabel.getBBox().width,
 				yTickLabelHeight: this.yTickLabel.getBBox().height,
-				startDate: this._estimates.firstIteration().startDate()._date,
 				iterationLength: this._estimates.firstIteration().length(),
 	            iterationCount: this._projections.totalIterations(),
 				maxEffort: this._projections.maxEffort()
