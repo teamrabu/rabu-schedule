80d700b8286fdf12eea7b3541e61f0a1a48ed803
James Shore <jshore@jamesshore.com>
2011-06-24 12:45:27 +0200

    Migrated Main to new class pattern

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,27 +1,38 @@
 // Copyright (C) 2011 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
 
-rabu.schedule.Main = function(config) {
+//var rs = rabu.schedule;
+//rs.Feature = function(feature, cumulativeEstimate, effortToDate) {
+//	this._feature = feature;
+//	this._cumulativeEstimate = cumulativeEstimate;
+//	this._effortToDate = effortToDate;
+//};
+//rs.Feature.prototype = new rs.Object();
+//var Feature = rs.Feature.prototype;
+
+(function() {
 	var rs = rabu.schedule;
-	
-	if (!config) {
-		throw "Expected config";
-	}
-	var estimates = new rs.Estimates(config);
-	var projections = new rs.Projections(estimates);
-	var featuresDom = new rs.FeaturesDom($(".rabu-features"), estimates);
-	var burnupDom = new rs.BurnupDom($(".rabu-burnup"), estimates, projections);
+	rs.Main = function(config) {
+		if (!config) {
+			throw "Expected config";
+		}
+		this._estimates = new rs.Estimates(config);
+		this._projections = new rs.Projections(this._estimates);
+		this._featuresDom = new rs.FeaturesDom($(".rabu-features"), this._estimates);
+		this._burnupDom = new rs.BurnupDom($(".rabu-burnup"), this._estimates, this._projections);
+	};
+	var Main = rs.Main.prototype = new rs.Object();
 
 	function dateToString(date) {
 		return date.toString('MMMM dS');
 	}
 
-	this.populateDom = function() {
-		$(".rabu-name").text(estimates.name());
-		$(".rabu-updated").text(estimates.updated().toString("MMMM dS, yyyy"));
-		$(".rabu-tenPercentDate").text(dateToString(projections.tenPercentDate()));
-		$(".rabu-fiftyPercentDate").text(dateToString(projections.fiftyPercentDate()));
-		$(".rabu-ninetyPercentDate").text(dateToString(projections.ninetyPercentDate()));
-		featuresDom.populate();
-		burnupDom.populate();
+	Main.populateDom = function() {
+		$(".rabu-name").text(this._estimates.name());
+		$(".rabu-updated").text(this._estimates.updated().toString("MMMM dS, yyyy"));
+		$(".rabu-tenPercentDate").text(dateToString(this._projections.tenPercentDate()));
+		$(".rabu-fiftyPercentDate").text(dateToString(this._projections.fiftyPercentDate()));
+		$(".rabu-ninetyPercentDate").text(dateToString(this._projections.ninetyPercentDate()));
+		this._featuresDom.populate();
+		this._burnupDom.populate();
 	};
-};
+}());
